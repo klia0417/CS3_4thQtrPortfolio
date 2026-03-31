552eb72fc7c5737e43099622480cbe9c3e74d4a4
klia0417 <b2029klialejandro@pshs.edu.ph>
2026-03-31 21:40:48 +0800

    new

---
+++ q4/3rdGraded/script.js
@@ -0,0 +1,29 @@
+function plotPoint(x0, y0, x, y) {
+    console.log(x0, y0, x, y);
+
+    document.getElementById("in1").innerHTML = x0 + " " + y0 + " " + x + " " + y;
+
+    var point = document.createElement('div');  
+    point.className = 'point';
+
+    point.style.left = (x - x0 + 200 - 5) + 'px';
+    point.style.bottom = (y - y0 + 200 - 5) + 'px';
+
+    document.getElementById('coordinatePlane').appendChild(point);
+
+    if (x > x0 && y > y0){
+      document.getElementById("out1").innerHTML = "NE";
+    }
+    else if (x < x0 && y < y0){
+      document.getElementById("out1").innerHTML = "SO";
+    }
+    else if (x > x0 && y < y0){
+      document.getElementById("out1").innerHTML = "SE";
+    }
+    else if (x < x0 && y > y0){
+      document.getElementById("out1").innerHTML = "NO";
+    }
+    else{
+      document.getElementById("out1").innerHTML = "divisa";
+    }
+}
